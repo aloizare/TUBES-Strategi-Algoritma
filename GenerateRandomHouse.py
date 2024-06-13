3527735993f6bf90b318ed68345e80dfe9ab1424
aloizare <168240032+aloizare@users.noreply.github.com>
2024-06-13 08:57:47 +0700

    Update GenerateRandomHouse.py

--- GenerateRandomHouse.py
+++ GenerateRandomHouse.py
@@ -3,9 +3,11 @@ import time
 
 def generate_random_houses(num_houses):
     houses = []
+    n=100                                    # ini adalah range number dari 1 hingga ke-n
     for i in range(num_houses):
-        houses.append(random.randint(1, 100))
+        houses.append(random.randint(1, n))
     return houses
 
-houses = generate_random_houses(1000)
+a=1000
+houses = generate_random_houses(a)            # a artinya mengenerate rumah sebanyak 1000 dengan range number dari 1 hinnga n, a bisa disesuaikan dengan jumlah rumah yang diinginkan
 print("Original houses:", houses)
